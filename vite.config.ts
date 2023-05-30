bd882be07caae9374b718ee3a81220f05ee59317
Arlagonix <gorbunov-sasha@yandex.ru>
2023-05-30 12:51:12 +0300

    fix the yml, vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,43 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  root: "src",
+  base: "./",
+  build: {
+    outDir: "../build",
+    emptyOutDir: true,
+    assetsDir: "assets",
+  },
+  server: {
+    open: true,
+  },
+  preview: {
+    open: true,
+  },
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      entry: "./index.tsx",
+      template: "./index.html",
+      inject: {
+        data: {
+          title: "index",
+          injectScript: `<script src="./index.tsx"></script>`,
+        },
+        tags: [
+          {
+            injectTo: "body-prepend",
+            tag: "div",
+            attrs: {
+              id: "tag",
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
